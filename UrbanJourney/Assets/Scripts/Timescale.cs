3dbc75bd4652f420c1f0434cb12fefdc24ee0739
Eric <bookjunkie93@gmail.com>
2020-05-02 11:21:23 -0400

    Timescale class

---
+++ UrbanJourney/Assets/Scripts/Timescale.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Monobehaviour that keeps track of time and modifies the game's
+/// timescale
+/// </summary>
+public class Timescale : MonoBehaviour
+{
+    [SerializeField] int nTick = 0;
+    [SerializeField] float fScale = 1f; //ticks/second
+    float elapsedTime = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public int GetTick()
+    {
+        return nTick;
+    }
+
+    public float GetScale()
+    {
+        return fScale;
+    }
+
+    public void SetTickScale(float i_newScale)
+    {
+        fScale = i_newScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        if(elapsedTime * fScale >= 1)
+        {
+            elapsedTime = 0;
+            nTick++;
+        }
+    }
+}
---
+++ UrbanJourney/Assets/Scripts/UITimescaleHUD.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITimescaleHUD : MonoBehaviour
+{
+    [SerializeField] Text currentTick;
+    [SerializeField] Text currentScale;
+    [SerializeField] Timescale timer;
+    [SerializeField] float scaleInterval = 0.1f;
+    void Awake()
+    {
+        timer = GameObject.FindObjectOfType<Timescale>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// Increment the timescale by the current scaleInterval
+    /// </summary>
+    public void IncrementScale()
+    {
+        timer.SetTickScale(timer.GetScale() + scaleInterval);
+    }
+
+    /// <summary>
+    /// Decrement the timescale by the current scaleInterval
+    /// </summary>
+    public void DecrementScale()
+    {
+        timer.SetTickScale(timer.GetScale() - scaleInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer != null)
+        {
+            currentTick.text= string.Format("Tick: {0}",  timer.GetTick().ToString());
+            currentScale.text= string.Format("Scale: {0}x", timer.GetScale().ToString());
+        }
+
+    }
+}
