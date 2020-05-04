399eebc909d8da7a161d1a9ca431bffea2ef78ed
Eric <bookjunkie93@gmail.com>
2020-05-04 12:24:47 -0400

    Spawner architecture, sample scene

---
+++ UrbanJourney/Assets/Scripts/Spawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] GameObject prefab;
+    [SerializeField] float cooldown = 5f;
+    Timescale timer;
+    GameObject instance;
+    float lastTick;
+    float timeElapsed =0;
+
+    void Awake()
+    {
+        timer = GameObject.FindObjectOfType<Timescale>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = Instantiate(prefab, transform) as GameObject;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(instance == null)
+        {
+            if(lastTick < timer.GetTick())
+            {
+                timeElapsed++;
+            }
+            if(timeElapsed >= cooldown)
+            {
+                timeElapsed = 0;
+                instance = Instantiate(prefab, transform) as GameObject;
+            }
+            lastTick = timer.GetTick();
+        }
+        
+    }
+}
